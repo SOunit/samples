44fdc47806ca8e58bdcac5db619c031fb2990b1d
SOUnit <workbookwork7@gmail.com>
2021-10-19 10:17:55 -0700

    add click event to damage button

--- 01_damage_animation/script.js
+++ 01_damage_animation/script.js
@@ -1,3 +1,12 @@
 (() => {
   console.log('hello from js');
+
+  // grab button element
+  const damageButton = document.querySelector('.js-damage-button');
+  console.log(damageButton);
+
+  // add event listener
+  damageButton.addEventListener('click', () => {
+    console.log('damage button click');
+  });
 })();
